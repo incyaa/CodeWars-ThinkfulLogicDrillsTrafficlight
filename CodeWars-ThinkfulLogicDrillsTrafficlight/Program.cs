0ef8b8256d9ed8f8842f97771730673695f127ba
Incyaa <incyaa@outlook.com>
2024-07-16 10:44:39 +0300

    Initial commit

---
+++ CodeWars-ThinkfulLogicDrillsTrafficlight/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeWars_ThinkfulLogicDrillsTrafficlight;
+
+class Program
+{
+     public static string UpdateLight(string current)
+        {
+            //Do Some Magic
+        }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
